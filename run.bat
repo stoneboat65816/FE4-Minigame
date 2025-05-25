70f7f811c68cf7f0e7702e25a48347b4b3d8c91f
stoneboat <asm650c816@gmail.com>
2025-05-25 15:42:43 +0700

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,102 @@
+copy dummy.bin FE4-minigame.smc
+copy dummy.bin event_00.bin
+copy dummy.bin event_01.bin
+copy dummy.bin kaiwa00.bin
+copy dummy.bin kaiwa01.bin
+copy dummy.bin kaiwa02.bin
+copy dummy.bin kaiwa03.bin
+copy dummy.bin kaiwa04.bin
+copy dummy.bin kaiwa05.bin
+copy dummy.bin kaiwa06.bin
+copy dummy.bin kaiwa07.bin
+copy dummy.bin kaiwa08.bin
+copy dummy.bin kaiwa09.bin
+copy dummy.bin kaiwa10.bin
+copy dummy.bin kaiwa11.bin
+copy dummy.bin kaiwa12.bin
+copy dummy.bin kaiwa13.bin
+copy dummy.bin kaiwa14.bin
+copy dummy.bin kaiwa15.bin
+copy dummy.bin kaiwa16.bin
+copy dummy.bin kaiwa17.bin
+copy dummy.bin kaiwa18.bin
+copy dummy.bin kaiwa19.bin
+copy dummy.bin kaiwa20.bin
+copy dummy.bin kaiwa21.bin
+copy dummy.bin kaiwa22.bin
+copy dummy.bin kaiwa23.bin
+copy dummy.bin kaiwa24.bin
+copy dummy.bin kaiwa25.bin
+copy dummy.bin kaiwa26.bin
+copy dummy.bin kaiwa27.bin
+copy dummy.bin kaiwa28.bin
+copy dummy.bin kaiwa29.bin
+copy dummy.bin kaiwa30.bin
+copy dummy.bin kaiwa31.bin
+copy dummy.bin kaiwa32.bin
+copy dummy.bin kaiwa33.bin
+copy dummy.bin kaiwa34.bin
+copy dummy.bin kaiwa35.bin
+copy dummy.bin kaiwa36.bin
+copy dummy.bin kaiwa37.bin
+copy dummy.bin kaiwa38.bin
+copy dummy.bin kaiwa39.bin
+copy dummy.bin kaiwa40.bin
+copy dummy.bin kaiwa41.bin
+copy dummy.bin kaiwa42.bin
+copy dummy.bin kaiwa43.bin
+copy dummy.bin kaiwa44.bin
+copy dummy.bin kaiwa45.bin
+
+Atlas -d log.txt event_00.bin event_script00.txt
+Atlas -d log.txt event_01.bin event_script01.txt
+atlas -d log.txt kaiwa00.bin kaiwa00.txt
+atlas -d log.txt kaiwa01.bin kaiwa01.txt
+atlas -d log.txt kaiwa02.bin kaiwa02.txt
+atlas -d log.txt kaiwa03.bin kaiwa03.txt
+atlas -d log.txt kaiwa04.bin kaiwa04.txt
+atlas -d log.txt kaiwa05.bin kaiwa05.txt
+atlas -d log.txt kaiwa06.bin kaiwa06.txt
+atlas -d log.txt kaiwa07.bin kaiwa07.txt
+atlas -d log.txt kaiwa08.bin kaiwa08.txt
+atlas -d log.txt kaiwa09.bin kaiwa09.txt
+atlas -d log.txt kaiwa10.bin kaiwa10.txt
+atlas -d log.txt kaiwa11.bin kaiwa11.txt
+atlas -d log.txt kaiwa12.bin kaiwa12.txt
+atlas -d log.txt kaiwa13.bin kaiwa13.txt
+atlas -d log.txt kaiwa14.bin kaiwa14.txt
+atlas -d log.txt kaiwa15.bin kaiwa15.txt
+atlas -d log.txt kaiwa16.bin kaiwa16.txt
+atlas -d log.txt kaiwa17.bin kaiwa17.txt
+atlas -d log.txt kaiwa18.bin kaiwa18.txt
+atlas -d log.txt kaiwa19.bin kaiwa19.txt
+atlas -d log.txt kaiwa20.bin kaiwa20.txt
+atlas -d log.txt kaiwa21.bin kaiwa21.txt
+atlas -d log.txt kaiwa22.bin kaiwa22.txt
+atlas -d log.txt kaiwa23.bin kaiwa23.txt
+atlas -d log.txt kaiwa24.bin kaiwa24.txt
+atlas -d log.txt kaiwa25.bin kaiwa25.txt
+atlas -d log.txt kaiwa26.bin kaiwa26.txt
+atlas -d log.txt kaiwa27.bin kaiwa27.txt
+atlas -d log.txt kaiwa28.bin kaiwa28.txt
+atlas -d log.txt kaiwa29.bin kaiwa29.txt
+atlas -d log.txt kaiwa30.bin kaiwa30.txt
+atlas -d log.txt kaiwa31.bin kaiwa31.txt
+atlas -d log.txt kaiwa32.bin kaiwa32.txt
+atlas -d log.txt kaiwa33.bin kaiwa33.txt
+atlas -d log.txt kaiwa34.bin kaiwa34.txt
+atlas -d log.txt kaiwa35.bin kaiwa35.txt
+atlas -d log.txt kaiwa36.bin kaiwa36.txt
+atlas -d log.txt kaiwa37.bin kaiwa37.txt
+atlas -d log.txt kaiwa38.bin kaiwa38.txt
+atlas -d log.txt kaiwa39.bin kaiwa39.txt
+atlas -d log.txt kaiwa40.bin kaiwa40.txt
+atlas -d log.txt kaiwa41.bin kaiwa41.txt
+atlas -d log.txt kaiwa42.bin kaiwa42.txt
+atlas -d log.txt kaiwa43.bin kaiwa43.txt
+atlas -d log.txt kaiwa44.bin kaiwa44.txt
+atlas -d log.txt kaiwa45.bin kaiwa45.txt
+
+xkas -o FE4-minigame.smc C_main.asm 
+pause
+
